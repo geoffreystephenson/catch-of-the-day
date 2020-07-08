ff8dacfce990b46926a11295f6acbb9c04ab36e7
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-07-07 21:18:36 -0400

    lessons 8-12

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
-import StorePicker from './components/StorePicker';
+import Router from './components/Router';
 import './css/style.css';
 
-render(<App />, document.getElementById('root'));
+render(<Router />, document.getElementById('root'));
