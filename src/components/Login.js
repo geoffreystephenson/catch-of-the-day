ecafcaadf6ab160ddfe2ac38cc7467a0f8b0a27b
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2020-07-14 22:57:08 -0400

    lesson 24

---
+++ src/components/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const Login = (props) => (
+	<nav className="login">
+		<h2>Inventory Login</h2>
+		<p>Sign in to manage your store's inventory</p>
+		<button className="github" onClick={() => props.authenticate('Github')}>
+			Login with Github
+		</button>
+		<button
+			className="facebook"
+			onClick={() => props.authenticate('Facebook')}
+		>
+			Login with Facebook
+		</button>
+		<button
+			className="twitter"
+			onClick={() => props.authenticate('Twitter')}
+		>
+			Login with Twitter
+		</button>
+	</nav>
+);
+
+Login.propTypes = {
+	authenticate: propTypes.func.isRequired,
+};
+
+export default Login;
